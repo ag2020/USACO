98668064c261eab82a0af1f997396b7cc2e63f7a
agawesome <33589152+agawesome@users.noreply.github.com>
2017-12-22 11:35:19 -0800

    Create ride.java

---
+++ Section 1.2/ride.java
@@ -0,0 +1,45 @@
+/*
+ID: 
+LANG: JAVA
+TASK: ride
+*/
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class ride {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("ride.in"));
+		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("ride.out")));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		String s = st.nextToken();
+		int productone = 1;
+		for (int i = 0; i < s.length(); i++) {
+			productone *= (s.charAt(i) - 64);
+		}
+		productone %= 47;
+		
+		st = new StringTokenizer(br.readLine());
+		s = st.nextToken();
+		int producttwo = 1;
+		for (int i = 0; i < s.length(); i++) {
+			producttwo *= (s.charAt(i) - 64);
+		}
+		producttwo %= 47;
+		
+		if (productone == producttwo) {
+			pw.println("GO");
+		} else {
+			pw.println("STAY");
+		}
+		
+		pw.close();
+	}
+}
